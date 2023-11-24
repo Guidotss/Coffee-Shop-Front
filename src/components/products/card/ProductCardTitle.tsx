feb33c0583ca5c99a26c9af12b0b0aac73ef503d
Guidotss <guidoolguin@hotmail.com>
2023-11-23 22:54:12 -0300

    Arreglando tamaños en notebook

--- src/components/products/card/ProductCardTitle.tsx
+++ src/components/products/card/ProductCardTitle.tsx
@@ -3,7 +3,7 @@ interface Props {
 }
 export const ProductCardTitle = ({ title }: Props) => {
   return (
-    <div className="absolute flex justify-center items-center w-[200px] h-[72px] mt-52 bg-black bg-opacity-70">
+    <div className="absolute flex justify-center items-center w-[200px] h-[72px] md:w-44 md:h-16 mt-52 md:mt-44 2xl:w-[200px] 2xl:mt-[13.5rem] bg-black bg-opacity-70">
       <h1 className="text-white text-xl">{title}</h1>
     </div>
   );
